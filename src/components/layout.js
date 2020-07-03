3a12935b9bb438050cb6f7d7c41cedcd05312143
Manuel Duran <mduran85@gmail.com>
2020-07-03 15:19:57 -0500

    init

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
+import NavBar from "../containers/NavBar"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
+import "../scss/main.scss"
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -25,21 +16,9 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <NavBar />
+      <main>{children}</main>
     </>
   )
 }
---
+++ src/containers/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+const NavBar = () => {
+  return (
+    <Navbar
+      className="navigation justify-content-end"
+      collapseOnSelect
+      expand="sm"
+    >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto mr-auto">
+          <Nav.Link className="navigation__link" href="#Home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="navigation__link" href="#FAQ">
+            FAQ
+          </Nav.Link>
+          <Nav.Link className="navigation__link" href="#Booking">
+            Booking
+          </Nav.Link>
+          <Nav.Link className="navigation__link" href="#Pricing-and-Policy">
+            Pricing and Policy
+          </Nav.Link>
+          <Nav.Link className="navigation__link" href="#About-Us">
+            About Us
+          </Nav.Link>
+          <Nav.Link className="navigation__link" href="#Blog">
+            Blog
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavBar
