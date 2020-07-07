534e412e4ebba8a7a181f4eb563b9e14b995f465
Manuel Duran <mduran85@gmail.com>
2020-07-07 09:24:51 -0500

    centered box contents

--- src/components/Services/Card.js
+++ src/components/Services/Card.js
@@ -1,12 +1,21 @@
 import React from "react"
 import BcgImage from "gatsby-background-image"
+import Img from "gatsby-image"
 
 const Card = ({ service: { image, body, title } }) => {
   return (
     <>
-      <BcgImage fixed={image.fixed} className="card">
-        <span>{title}</span>
-      </BcgImage>
+      <div className="service__card">
+        <div className="service__card__inner">
+          <Img fixed={image.fixed} className="service__card__image" />
+        </div>
+        <div className="service__card__title">
+          <span>{title}</span>
+        </div>
+        <div className="service__card__body">
+          <span>{body}</span>
+        </div>
+      </div>
     </>
   )
 }
