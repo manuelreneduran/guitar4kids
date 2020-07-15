887a300a10e74545d9585d2a2f039ac3ea983059
Manuel Duran <mduran85@gmail.com>
2020-07-15 11:19:54 -0500

    styled page hero image with clip path polygon

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,13 +3,15 @@ import BcgImage from "gatsby-background-image"
 import Container from "react-bootstrap/Container"
 const Hero = ({ img, home, children }) => {
   return (
-    <BcgImage
-      className={home ? "hero hero__home" : "hero hero__page"}
-      fluid={img}
-      home={home}
-    >
-      {children}
-    </BcgImage>
+    <div className="hero__wrapper">
+      <BcgImage
+        className={home ? "hero hero__home" : "hero hero__page"}
+        fluid={img}
+        home={home}
+      >
+        {children}
+      </BcgImage>
+    </div>
   )
 }
 
